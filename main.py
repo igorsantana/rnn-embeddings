0e6595a3988a39a3332c0234e064d8983cd7450a
Igor Santana <igorpsant95@gmail.com>
2019-09-04 15:23:24 -0300

    before merge

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import yaml
-import numba
 import numpy as np
 import multiprocessing                      as mp
 import project.data.preprocess              as pp
@@ -17,21 +16,8 @@ if __name__ == '__main__':
     printlog('Starting the process, reading the configuration file.')
     conf = yaml.safe_load(open('config.yml'))
 
-#     # # mp.set_start_method('spawn')
-
     pp.preprocess(conf['evaluation']['dataset'], conf['session']['interval'])
 
     emb.model_runner(conf['evaluation']['dataset'], conf['models']['music2vec'])
 
     r.execute_cv(conf['evaluation'])
-
-    
-
-    # u = np.random.rand(100)
-    # print(u)
-    # print(u.dtype)
-    # M = np.random.rand(100000, 100)
-    # print(M)
-    # print(M.dtype)
-
-    # print(fast_cosine_matrix(u, M))
