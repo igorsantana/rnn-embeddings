f8d113be25e91c3d64d0b135ad7d1e795b9d13a6
Igor Santana <igorpsant95@gmail.com>
2019-09-27 10:19:20 -0300

    results

--- results.py
+++ results.py
@@ -9,7 +9,7 @@ np.set_printoptions(suppress=True)
 df          = pd.read_csv('tmp/xiami-small/results.csv', header=None)
 df.columns  = ['config', 'algo', 'prec', 'rec', 'f1']
 
-colors      = sns.color_palette("colorblind", 4)
+colors      = sns.color_palette("colorblind", 100)
 
 
 f = open('tmp/xiami-small/id_emb.csv', 'r')
@@ -19,15 +19,16 @@ methods = '[{}]'.format(methods.replace('"', ''))
 methods = eval(methods)
 methods = dict(methods)
 
-glove = []
-barWidth = 0.2
-cols = ['config','window', 'dim', 'lr', 'epochs']
+m2v = []
+barWidth = 0.015
+cols = ['config','window', 'dim', 'lr', 'epochs', 'down', 'neg']
 
 for key in methods.keys():
-    if 'glove' in key:
-        glove.append([key] + [float(k.split('=')[1]) for k in methods[key].split(';')])
+    if 'm2v' in key:
+        m2v.append([key] + [float(k.split('=')[1]) for k in methods[key].split(';')])
 
-confs = pd.DataFrame(glove, columns= cols)
+
+confs = pd.DataFrame(m2v, columns= cols)
 confs.index = confs.config
 confs = confs.drop('config', 1)
 
@@ -35,12 +36,12 @@ confs = confs.drop('config', 1)
 df = df.merge(confs, how='left', left_on='config', right_index=True)
 df['emb']   = df.config.apply(lambda x: x.split('_')[0])
 df['id']    = df.config.apply(lambda x: int(x.split('_')[1]))
-df          = df[df.emb == 'glove']
+df          = df[df.emb == 'm2v']
 df          = df.drop(['emb', 'id'], axis=1)
-
 df['window'] = df.window.apply(int)
 df['dim'] = df.dim.apply(int)
 df['epochs'] = df.epochs.apply(int)
+df['neg'] = df.neg.apply(int)
 
 m2vtn   = df[df.algo == 'm2vTN']
 sm2vtn  = df[df.algo == 'sm2vTN']
@@ -55,47 +56,49 @@ j = 0
 __labels = []
 
 fig, axes   = plt.subplots(2, 2)
-# [0.010, 0.015, 0.020, 0.025]
 
 for i in range(2):
     for j in range(2):
         ax = axes[i, j]
         
-        data = values[i][j][['lr', 'f1', 'config']].sort_values(by='lr').values
+        data = values[i][j][['neg', 'f1', 'config']].sort_values(by='neg').values
         x = data[:,0]
+        n_values = np.unique(x)
         y = data[:,1]
+        
         labels = data[:,2]
         __labels = labels
-        y50  = []
-        y100 = []
-        y150 = []
-        y200 = []
 
-        x50 = np.arange(x.shape[0] // 4)
 
-        for k in range(x.shape[0] // 4):
-            y50.append(y[k])
-            y100.append(y[k] * 2)
-            y150.append(y[k] * 3)
-            y200.append(y[k] * 4)
+        ind     = np.arange(n_values.shape[0])
+        ind2    = np.arange(n_values.shape[0])
 
-        ax.fill_between(x50, y200, color=colors[0], label='0.025', alpha=0.2)
-        ax.fill_between(x50, y150, color=colors[1], label='0.02', alpha=0.2)
-        ax.fill_between(x50, y100, color=colors[2], label='0.015', alpha=0.2)
-        ax.fill_between(x50, y50, color=colors[3], label='0.01', alpha=0.2)
+        ax.xaxis.set_ticks(ind)
+        ax.xaxis.set_ticklabels(n_values)
 
-        ax.plot(x50, y200, color=colors[0],linewidth=1)
-        ax.plot(x50, y150, color=colors[1],linewidth=1)
-        ax.plot(x50, y100, color=colors[2],linewidth=1)
-        ax.plot(x50, y50, color=colors[3],linewidth=1)
+        ax.bar(ind, [y[0], y[0] * 2, y[0] * 3, y[0] * 4, y[0] * 5], width=barWidth, color=colors[7])
+        fg =  (x.shape[0] // n_values.shape[0]) // 2
+        c = 0
+        for k in range(1, fg):
+            ax.bar(ind2 - barWidth, [y[k], y[k] * 2, y[k] * 3, y[k] * 4, y[k] * 5], width=barWidth, color=colors[c])
+            ind2 = np.array([x - barWidth for x in ind2])
+            c+=1
+
+        for k in range(fg, fg * 2):
+            ax.bar(ind + barWidth, [y[k], y[k] * 2, y[k] * 3, y[k] * 4, y[k] * 5], width=barWidth, color=colors[c])
+            ind = np.array([x + barWidth for x in ind])
+            c+=1
+
         ax.set_title(titles[i][j])
         ax.legend(loc='upper right')
-        plt.setp(ax.get_xticklabels(), rotation='vertical', fontsize=10)
+        plt.setp(ax.get_xticklabels(), fontsize=10)
         
 
-plt.setp(axes, xticks=[r for r in range(len(__labels) // 4)], xticklabels=__labels, yticks=[x / 100 for x in np.arange(0, 101, 20)])
-plt.legend()
+
+plt.setp(axes, yticks=[x / 100 for x in np.arange(0, 101, 5)])
+# plt.legend()
 plt.tight_layout()
+plt.suptitle('Resultados relativos ao número de dimensões utilizada para se obter os resultados')
 plt.show()
 
 
